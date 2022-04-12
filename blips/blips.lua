03be5b15ff8838b306ca8735c1c3e868933880aa
jg-white@outlook.com <XGTDuZCfG6>
2022-04-13 00:17:37 +0100

    Coords, truck map icon + spawnpoint

---
+++ blips/blips.lua
@@ -0,0 +1,20 @@
+local blips = {
+    -- Example {title="", colour=, id=, x=, y=, z=},
+
+     {title="Trucking", colour=9, id=477, x = 123.19, y = 6406.92, z = 31.36}
+  }
+      
+Citizen.CreateThread(function()
+
+    for _, info in pairs(blips) do
+      info.blip = AddBlipForCoord(info.x, info.y, info.z)
+      SetBlipSprite(info.blip, info.id)
+      SetBlipDisplay(info.blip, 4)
+      SetBlipScale(info.blip, 1.0)
+      SetBlipColour(info.blip, info.colour)
+      SetBlipAsShortRange(info.blip, true)
+	  BeginTextCommandSetBlipName("STRING")
+      AddTextComponentString(info.title)
+      EndTextCommandSetBlipName(info.blip)
+    end
+end)
